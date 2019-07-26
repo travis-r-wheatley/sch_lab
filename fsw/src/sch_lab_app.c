a5fc95f3edf237594e5b4ce636690e232307ed2a
Jake Hageman <jacob.hageman@nasa.gov>
2019-07-26 15:35:52 -0400

    Replace deprecated cfe refs, #6

--- fsw/src/sch_lab_app.c
+++ fsw/src/sch_lab_app.c
@@ -72,7 +72,7 @@ void SCH_Lab_AppMain(void)
     int              i;
     int32            rtnStat;
     uint32           SCH_OneHzPktsRcvd = 0;
-    uint32           RunStatus = CFE_ES_APP_RUN;
+    uint32           RunStatus = CFE_ES_RunStatus_APP_RUN;
     CFE_SB_PipeId_t  SCH_CmdPipe;
 
     CFE_ES_PerfLogEntry(SCH_MAIN_TASK_PERF_ID);
